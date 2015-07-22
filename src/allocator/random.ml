d4807454549c905482f8555bfdd7974564dc52a5
Glenn Y. Rolland <glenux@glenux.net>
2015-07-22 23:17:35 +0200

    Add lib.

---
+++ src/allocator/random.ml
@@ -0,0 +1,3 @@
+(* choose an random position on the smallest non-filled square *)
+
+
---
+++ src/allocator/spiral.ml
@@ -0,0 +1,17 @@
+
+type vector = {
+  x: int ;
+  y: int
+}
+
+let initialize x y = begin
+    ()
+end
+ 
+let rotate_left { x ; y } = begin
+    { x = b ; y = -a }
+end
+
+let rotate_right { a ; b } = begin
+    { x= -b, y = a }
+end
